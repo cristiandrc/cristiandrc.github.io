21f7a84a18ce28bc229fc5f398498da0f101b5e3
cristian <crisdarroca@hotmail.com>
2021-08-31 21:57:52 -0500

    add copyPlugin webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -48,6 +49,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./public/_redirects", to: "" }],
     }),
   ],
 
